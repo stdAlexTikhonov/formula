2dcb969fd6cab7047dfd6773f4f991a6eb6b0914
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-08-10 11:48:38 +0300

    List code refactor

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,26 +1,16 @@
 import React from "react";
-import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import InboxIcon from "@material-ui/icons/Inbox";
 import { useAppDispatch, useAppSelector } from "../../hooks";
-import { setCode, getCode, getCurrentIndex } from "../../store/codeSlice";
+import { setCode, getCurrentIndex } from "../../store/codeSlice";
 import Tree from "../../Tree";
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: "100%",
-      backgroundColor: theme.palette.background.paper,
-    },
-  })
-);
+import { useStyles } from "./styles";
 
 export default function SelectedListItem(props: any) {
   const dispatch = useAppDispatch();
-  const code = useAppSelector(getCode);
   const index_in_tree = useAppSelector(getCurrentIndex);
   const classes = useStyles();
   const [selectedIndex, setSelectedIndex] = React.useState(1);
@@ -36,15 +26,12 @@ export default function SelectedListItem(props: any) {
     if (node) {
       node.value = value;
       node.type = props.type === "functions" ? "FUNCTION" : "OPERAND";
-      //Функции
+
       if (props.type === "functions") {
         node.setLeft();
         node.setRight();
       }
     }
-
-    // if (props.type === "Функции") dispatch(setCode(code + `${value}(`));
-    // else dispatch(setCode(code + `[${value}] `));
   };
 
   return (
---
+++ src/components/List/styles.ts
@@ -0,0 +1,10 @@
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+      backgroundColor: theme.palette.background.paper,
+    },
+  })
+);
