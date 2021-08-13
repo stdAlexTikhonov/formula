0791160b8005183c2bec1b19addd1469b15cb54b
Alexander <stdAlexTikhonov@hotmail.com>
2021-08-13 17:25:03 +0300

    fix for public

--- src/components/CustomIcon/index.tsx
+++ src/components/CustomIcon/index.tsx
@@ -17,25 +17,25 @@ export const CustomIcon: React.FC<Props> = ({ value }) => {
   const getIcon = (value: string) => {
     switch (value) {
       case "+":
-        return <img src={plus} width={20} height={20} alt="+" />;
+        return <img src={"." + plus} width={20} height={20} alt="+" />;
       case "-":
-        return <img src={minus} width={20} height={20} alt="-" />;
+        return <img src={"." + minus} width={20} height={20} alt="-" />;
       case "<":
-        return <img src={less} width={20} height={20} alt="<" />;
+        return <img src={"." + less} width={20} height={20} alt="<" />;
       case "<=":
-        return <img src={less_or_eq} width={20} height={20} alt="<=" />;
+        return <img src={"." + less_or_eq} width={20} height={20} alt="<=" />;
       case ">":
-        return <img src={greater} width={20} height={20} alt=">" />;
+        return <img src={"." + greater} width={20} height={20} alt=">" />;
       case ">=":
-        return <img src={gre_or_eq} width={20} height={20} alt=">=" />;
+        return <img src={"." + gre_or_eq} width={20} height={20} alt=">=" />;
       case "!=":
-        return <img src={not_eq} width={20} height={20} alt="!=" />;
+        return <img src={"." + not_eq} width={20} height={20} alt="!=" />;
       case "==":
-        return <img src={eq} width={20} height={20} alt="==" />;
+        return <img src={"." + eq} width={20} height={20} alt="==" />;
       case "*":
-        return <img src={mult} width={20} height={20} alt="*" />;
+        return <img src={"." + mult} width={20} height={20} alt="*" />;
       case "/":
-        return <img src={divide} width={20} height={20} alt="/" />;
+        return <img src={"." + divide} width={20} height={20} alt="/" />;
       default:
         return value;
     }
